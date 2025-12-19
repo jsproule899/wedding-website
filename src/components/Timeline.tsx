5618f16ad61b73dbb194ae165133a2deec78f018
Joshua Sproule <joshsproule@live.co.uk>
2025-12-19 15:24:25 +0000

    Feat:  Timeline page updates

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -1,21 +1,35 @@
 import FadeSection from "./FadeSection"
 import Footer from "./Footer"
 
+interface pointProps {
+    time: string;
+    event: string;
+    end?: boolean;
+}
+
+const TimelinePoint = ({ time, event, end }: pointProps) => (
+    <>
+        <span className="col-span-2 justify-self-end content-center">{time}</span>
+        <span className={`bg-white rounded-full w-4 h-4 content-center ${end ? "" : "after:block after:bg-white after:w-0.5 after:h-16 after:mx-auto after:my-2"}`} />
+        <span className="col-span-2 justify-self-start text-start content-center h-8">{event}</span>
+    </>
+)
+
+
 function Timeline() {
     return (
         <FadeSection id='Timeline' className="justify-center space-y-5">
-           <div className="flex flex-col grow space-y-4 w-full justify-center items-center">
-                <h1 className="text-7xl/12 ">On the<br />Day</h1>
-                <div className="relative h-8/12 w-10/12">
-                    <div className="h-10/12 w-0 right-0 left-0 mx-auto border border-white"></div>
-                    <div className="absolute top-10 left-1/2"><hr className="border" /> Ceremony - 2.00pm</div>
-                    <div className="absolute top-30 right-1/2"><hr className="border" /><p>Drinks Reception - 2.30pm</p></div>
-                    <div className="absolute top-50 left-1/2"><hr className="border" /><p>Speeches- 5.00pm</p></div>
-                    <div className="absolute top-70 right-1/2"><hr className="border" /><p>Dinner - 5.30pm</p></div>
-                    <div className="absolute top-90 left-1/2"><hr className="border" /><p>First Dance- 7.00pm</p></div>
-                    <div className="absolute top-120 right-1/2"><hr className="border" /><p>Party- 10.30pm - late</p></div>
+            <div className="flex flex-col grow space-y-4 w-full justify-center items-center">
+                <h1 className="text-7xl/12 mb-10 ">On the<br />Day</h1>
+                <div className="grid grid-cols-5 gap-x-1 gap-y-8 justify-items-center items-center text-xl font-minerva mx-10">
+                    <TimelinePoint time="2.00pm" event="Ceramony" />
+                    <TimelinePoint time="3.00pm" event="Drinks Reception" />
+                    <TimelinePoint time="5.00pm" event="Speeches" />
+                    <TimelinePoint time="5.30pm" event="Dinner" />
+                    <TimelinePoint time="7.00pm" event="First Dance" />
+                    <TimelinePoint time="10.30pm" event="DJ & Dancing" />
+                    <TimelinePoint time="1.00 am" event="BedTime" end />
                 </div>
-
             </div>
             <Footer />
         </FadeSection>

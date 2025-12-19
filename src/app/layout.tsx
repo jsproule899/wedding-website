7bde95dd26b099b8b094488040da1af4342cc22e
Joshua Sproule <joshsproule@live.co.uk>
2025-12-19 15:47:41 +0000

    Refactor: Add Suspense

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import "./globals.css";
+import { Suspense } from 'react';
 
 export const metadata: Metadata = {
     title: 'Rachael & Josh Wedding',
@@ -14,7 +15,11 @@ export default function RootLayout({
     return (
         <html lang="en">
             <body>
-                <div id="root">{children}</div>
+                <div id="root">
+                    <Suspense fallback={<div>loading</div>}>
+                        {children}
+                    </Suspense>
+                </div>
             </body>
         </html>
     )

56df474b40c68b5ed04724105964363a045c8aa9
Joshua Sproule <joshsproule@live.co.uk>
2025-12-19 15:23:36 +0000

    Fix: Hide mobile browser bar on scroll

--- src/components/FadeSection.tsx
+++ src/components/FadeSection.tsx
@@ -7,7 +7,7 @@ export default function FadeSection({ id, className, children }: { id?: string; 
     <section
       id={id}
       ref={ref}
-      className={`h-svh flex flex-col items-center justify-between transition-opacity ease-in duration-700 snap-start snap-always py-10 ${inView ? "opacity-100" : "opacity-0"} ${className ?? ""}`}
+      className={`h-dvh flex flex-col items-center justify-between transition-opacity ease-in-out duration-700 snap-start snap-always py-10 ${inView ? "opacity-100" : "opacity-0"} ${className ?? ""}`}
     >
       {children}
     </section>

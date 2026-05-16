0a385005836217a2e3f8db94cb62fe0bc5b3e4ff
Joshua Sproule <joshsproule@live.co.uk>
2026-05-16 17:13:10 +0100

    Style: Timeline icons

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -1,16 +1,18 @@
 import FadeSection from "./ui/FadeSection"
 import Footer from "./ui/Footer"
+import { GiLinkedRings, GiGlassCelebration   , GiMicrophone, GiHotMeal, GiMusicalNotes, GiGuitar, GiCompactDisc, GiNightSleep } from "react-icons/gi";
 
 interface pointProps {
     time: string;
     event: string;
     end?: boolean;
+    icon?: React.ReactNode;
 }
 
-const TimelinePoint = ({ time, event, end }: pointProps) => (
+const TimelinePoint = ({ time, event, end, icon }: pointProps) => (
     <>
         <span className="col-span-2 justify-self-end content-center">{time}</span>
-        <span className={`bg-white rounded-full w-2.5 h-2.5 content-center ${end ? "" : "after:block after:bg-white after:w-0.5 after:h-8 xs:after:h-12  after:mx-auto after:my-2"}`} />
+        <span className={`w-7.5 h-7.5 text-3xl ${end ? "" : "after:block after:bg-white after:w-0.5 after:h-8 xs:after:h-5 after:mx-auto after:my-0.5"}`} >{icon}</span>
         <span className="col-span-2 justify-self-start text-start content-center h-6">{event}</span>
     </>
 )
@@ -22,14 +24,14 @@ function Timeline() {
             <div className="flex flex-col grow xs:space-y-4 w-full justify-center items-center">
                 <h1 className="text-5xl/8 xs:text-7xl/12 mb-4 xs:mb-10 ">On the<br />Day</h1>
                 <div className="grid grid-cols-5 gap-x-1 gap-y-3 xs:gap-y-6 justify-items-center items-center text-sm xs:text-lg font-minerva mx-10">
-                    <TimelinePoint time="1:30pm" event="Ceremony" />
-                    <TimelinePoint time="2:15pm" event="Drinks & Cake" />
-                    <TimelinePoint time="4:30pm" event="Speeches" />
-                    <TimelinePoint time="5:00pm" event="Dinner" />
-                    <TimelinePoint time="7:00pm" event="First Dance" />
-                    <TimelinePoint time="7:15pm" event="Band" />
-                    <TimelinePoint time="10:30pm" event="DJ & Dancing" />
-                    <TimelinePoint time="1:00 am" event="Bed Time" end />
+                    <TimelinePoint time="1:30pm" event="Ceremony" icon={<GiLinkedRings />} />
+                    <TimelinePoint time="2:15pm" event="Drinks & Cake" icon={<GiGlassCelebration    />} />
+                    <TimelinePoint time="4:30pm" event="Speeches" icon={<GiMicrophone />} />
+                    <TimelinePoint time="5:00pm" event="Dinner" icon={<GiHotMeal />} />
+                    <TimelinePoint time="7:00pm" event="First Dance" icon={<GiMusicalNotes />} />
+                    <TimelinePoint time="7:15pm" event="Band" icon={<GiGuitar />} />
+                    <TimelinePoint time="10:30pm" event="DJ & Dancing" icon={<GiCompactDisc />} />
+                    <TimelinePoint time="1:00 am" event="Bed Time" end  icon={<GiNightSleep />} />
                 </div>
             </div>
             <Footer />
